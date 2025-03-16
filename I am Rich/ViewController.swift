2301e88fba0030a3a68c5e4aa5875f7a6dbdbf39
kalinduagathisi <kalinduagathisi@gmail.com>
2025-03-16 14:56:31 +0530

    played around with storyborad files and added img, text widgets

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Kalindu Agathisi on 2025-02-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
